75c9200bebc1a3b568a38800084475a3c11101d5
Erik Griffin <griffin.erik@gmail.com>
2015-01-28 22:15:12 +0100

    switched to hash implementation

--- directory.rb
+++ directory.rb
@@ -1,5 +1,9 @@
 
-student_directory = ["Martha","Marcy","Marlene","and me"]
+student_directory = [
+  {name: "Martha", cohort: "February"},
+  {name: "Marcy", cohort: "February"},
+  {name: "Marlene", cohort: "February"},
+  {name: "and me", cohort: "February"}]
 
 # I think this particular use of methods makes the code verbose
 # and less clear.
@@ -10,23 +14,25 @@ puts "--------------------------------"
 end
 
 
-def printEach(items)
+def printEach(students)
 # is there a significance of using .each do |x|
 # versus .each { |x| }  ?
 # other than that you have to use 'end' of course.
 
-  items.each {|item|
-    puts item
+  students.each {|student|
+    puts "#{student[:name]} from #{student[:cohort]} cohort"
   }
 end
 
-def printFooter(items)
+def printFooter(students)
 # Decided its cleaner to put it first into a variable
 # Remember it's not a string, but the interpolation is
 # showing the number.
-  student_count = items.count
+  student_count = students.count
 puts "#{student_count} students in total."
 end
+
+## Program Start
 
 printHeader
 printEach(student_directory)
