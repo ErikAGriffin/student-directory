707239ab4ab505ac7387341d1ac0ad2a1a363a6d
Erik Griffin <griffin.erik@gmail.com>
2015-01-28 21:52:52 +0100

    Made use of methods

--- directory.rb
+++ directory.rb
@@ -1,6 +1,34 @@
 
 student_directory = ["Martha","Marcy","Marlene","and me"]
 
-student_directory.each {|student|
-  puts student
+# I think this particular use of methods makes the code verbose
+# and less clear.
+
+def printHeader
+puts "Current Maker's Academy Students"
+puts "--------------------------------"
+end
+
+
+def printEach(items)
+# is there a significance of using .each do |x|
+# versus .each { |x| }  ?
+# other than that you have to use 'end' of course.
+
+  items.each {|item|
+    puts item
   }
+end
+
+def printFooter(items)
+# Decided its cleaner to put it first into a variable
+# Remember it's not a string, but the interpolation is
+# showing the number.
+  student_count = items.count
+puts "#{student_count} students in total."
+end
+
+printHeader
+printEach(student_directory)
+printFooter(student_directory)
+
