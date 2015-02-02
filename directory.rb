cd51a833ff96507fce97d247fccbd52cd7806ac5
Erik Griffin <griffin.erik@gmail.com>
2015-02-02 00:03:33 +0000

    Added basic interactive menu

--- directory.rb
+++ directory.rb
@@ -5,12 +5,44 @@ student_directory = [
   {name: "Marlene", cohort: "February"},
   {name: "and me", cohort: "February"}]
 
+def show_menu
+
+  directory = []
+
+  loop do
+    puts "Student Directory".center(30,'..')
+    puts "Menu...".center(30,'..')
+    puts "1. Add Students"
+    puts "2. Print Student Directory"
+    #puts "3. "
+    #puts "4. "
+    puts "0. Exit"
+
+    input = gets.chomp
+
+
+    case input
+
+      when '1'
+        directory << input_students
+      when '2'
+        print_all(directory.flatten!)
+      #when '3'
+      #when '4'
+      when '0',""
+      break
+      else
+        puts "Sorry, #{input} isn't a valid choice."
+    end
+
+
+  end
+end
 
 
 
-def inputStudents
+def input_students
 
-  puts ""
   puts "Please enter the student's name to add them to the directory"
   puts "When done enter a blank line"
 
@@ -22,7 +54,13 @@ def inputStudents
 
     students << {name: student, cohort: "February"}
 
-    puts "We now have #{students.count} students. How lovely."
+    if students.count == 1
+      puts "We now have #{students.count} new student. How lovely."
+    else
+      puts "We now have #{students.count} new students. How lovely."
+    end
+
+
 
     student = gets.chomp
   end
@@ -35,32 +73,30 @@ def inputStudents
 end
 
 
-def printHeader
+def print_header
 puts "Current Maker's Academy Students"
 puts "--------------------------------"
-  puts "Who's name begin with 'M'"
 end
 
 
-def printEach(students)
+  def print_all(students)
 # is there a significance of using .each do |x|
 # versus .each { |x| }  ?
 # other than that you have to use 'end' of course.
+    # Note: removed the 'M' only functionality from previous exercise.
 
-
+  print_header
   i = 1
-
   students.each {|student|
-    if student[:name].capitalize[0] != "M"
-      #do nothing
-    else
     puts "#{i}. #{student[:name]} from #{student[:cohort]} cohort"
     i+=1
-    end
   }
+print_footer(students)
+puts "Press enter to return to menu"
+gets
 end
 
-def printFooter(students)
+def print_footer(students)
 # Decided its cleaner to put it first into a variable
 # Remember it's not a string, but the interpolation is
 # showing the number.
@@ -68,10 +104,8 @@ def printFooter(students)
   puts "There are #{student_count} students in total."
 end
 
-## Program Start
+## Program Start ##
 
-students = inputStudents
-printHeader
-printEach(students)
-printFooter(students)
+
+show_menu
 
